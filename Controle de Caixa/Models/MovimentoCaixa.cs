dc99965be4872ee24df02f6f45b8d2631f9f83dd
ITALO PIOVAN <italo_piovan@hotmail.com>
2021-11-03 19:22:29 -0300

    projeto criado

---
+++ Controle de Caixa/Models/MovimentoCaixa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle_de_Caixa.Models
+{
+    public class MovimentoCaixa
+    {
+        int _id;
+        Caixa _caixa;
+        AcertoCaixa _acerto;
+        decimal _valor;
+        bool _tipo;
+
+        public Caixa Caixa { get => _caixa; set => _caixa = value; }
+        public AcertoCaixa Acerto { get => _acerto; set => _acerto = value; }
+        public decimal Valor { get => _valor; set => _valor = value; }
+        public bool Tipo { get => _tipo; set => _tipo = value; }
+        public int Id { get => _id; set => _id = value; }
+    }
+}
