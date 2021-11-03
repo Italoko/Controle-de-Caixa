a9445ccea17257c74bcf9071bc6bec9575ce6eca
ITALO PIOVAN <italo_piovan@hotmail.com>
2021-11-03 19:24:45 -0300

    CONTROL

---
+++ Controle de Caixa/Controllers/CaixaController.cs
@@ -0,0 +1,19 @@
+﻿using Controle_de_Caixa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle_de_Caixa.Controllers
+{
+    public class CaixaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
