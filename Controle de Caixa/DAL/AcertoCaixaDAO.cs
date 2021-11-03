a9ebfd76457ca7cf0d6839333394ca1178bdcb31
ITALO PIOVAN <italo_piovan@hotmail.com>
2021-11-03 19:24:32 -0300

    DAOS

---
+++ Controle de Caixa/DAL/AcertoCaixaDAO.cs
@@ -0,0 +1,46 @@
+﻿using Controle_de_Caixa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle_de_Caixa.DAL
+{
+    public class AcertoCaixaDAO
+    {
+        MySQLPersistence _bd = new MySQLPersistence();
+        private static AcertoCaixaDAO instance;
+        private AcertoCaixaDAO() { }
+        public static AcertoCaixaDAO getInstance()
+        {
+            if (instance == null)
+                instance = new AcertoCaixaDAO();
+            return instance;
+        }
+
+        public (int, string) Gravar(AcertoCaixa acertoCaixa)
+        {
+            int linhas = 0;
+            string msg = "";
+
+            try
+            {
+                string sql = "insert into acerto_caixa (data, tipo, valor,motivo)" +
+                "values (@data, @tipo, @valor, @motivo)";
+
+                _bd.LimparParametros();
+                _bd.AdicionarParametro("@data",acertoCaixa.Data);
+                _bd.AdicionarParametro("@tipo", acertoCaixa.Tipo);
+                _bd.AdicionarParametro("@valor", acertoCaixa.Valor);
+                _bd.AdicionarParametro("@motivo", acertoCaixa.Motivo);
+
+                _bd.AbrirConexao();
+                linhas = _bd.ExecutarNonQuery(sql);
+                _bd.FecharConexao();
+            }
+            catch (Exception ex)
+            { msg = "Não foi possível salvar. Tente novamente."; }
+            return (linhas, msg);
+        }
+    }
+}
---
+++ Controle de Caixa/DAL/MySQLPersistence.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Controle_de_Caixa.DAL
+{
+    public class MySQLPersistence
+    {
+        string _strCon = ""; 
+        MySqlConnection _conexao; 
+        MySqlCommand _comando;
+        public MySQLPersistence() 
+        {
+            _strCon = "Data Source = den1.mysql4.gear.host; Database = aulalp4; User Id = aulalp4; Password = Al59dz6?6v7_; ; SSL Mode = None";
+            _conexao = new MySqlConnection(_strCon);
+            _comando = _conexao.CreateCommand(); 
+        }
+
+        public void AbrirConexao()
+        {
+            if (_conexao.State != System.Data.ConnectionState.Open)  
+                _conexao.Open(); 
+        }
+
+        public void FecharConexao()
+        {
+            _conexao.Close();
+        }
+
+
+        public void AdicionarParametro(string nome, object valor)
+        {
+            _comando.Parameters.AddWithValue(nome, valor);
+        }
+
+        public void LimparParametros()
+        {
+            _comando.Parameters.Clear();  
+        }
+                                                  
+        public int ExecutarNonQuery(string instrucao, Dictionary<string, object> parametros = null)// Executa os comandos de NonQuery
+        {
+            _comando.CommandText = instrucao;
+            if (parametros != null)
+            {
+                foreach (var item in parametros)
+                {
+                    _comando.Parameters.AddWithValue(item.Key, item.Value);
+                }
+            }
+            int linhasAfetadas = _comando.ExecuteNonQuery();
+            return linhasAfetadas; 
+        }
+    }
+}
